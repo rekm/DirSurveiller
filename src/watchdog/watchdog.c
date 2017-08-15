0fa841f2ac356e7630ce4fdfd1b08b66577c93bd
René Kmiecinski <r.w.kmiecinski@gmail.com>
2017-08-15 18:06:10 +0200

    Written getter for max_PID from system

--- src/watchdog/watchdog.c
+++ src/watchdog/watchdog.c
@@ -4,7 +4,46 @@
 #include <signal.h>
 
 
-int main(void){
+/**
+ * Gets max pid from proc pseudo filesystem under linux  
+ */
+int get_max_PID(){
+    int ret = 0;
+    FILE* pid_maxf;
+    //reading /proc/sys/kernel/pid_max
     
-    return 0;
+    char line[100];
+    pid_maxf = fopen("/proc/sys/kernel/pid_max","r");
+    if (!pid_maxf){
+        ret = -1;
+        goto endfun;
+    }
+    if (!fgets(line, 100, pid_maxf)){ 
+        ret = -1;
+        goto closeFile;
+    }
+    ret = atoi(line);
+
+closeFile:
+    fclose(pid_maxf);
+endfun:
+    return ret;
+
+}
+
+
+
+
+
+int main(void){
+    int max_pid; 
+
+    max_pid = get_max_PID();
+    if (max_pid == -1) {
+        fprintf(stderr, "Could not read /proc/sys/kernel/pid_max\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    fprintf(stderr, "Max PID: %i\n",max_pid); 
+    exit(EXIT_SUCCESS);
 }
